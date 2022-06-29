ac593a7874b9c4d279c722042e457d5534f3623a
Nicolás Prieto <npcstalker@gmail.com>
2022-06-29 17:27:45 +0200

    second commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using TodoAPI.Interfaces;
+using TodoAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ITodoRepository, TodoRepository>();
 
 var app = builder.Build();
 
@@ -15,8 +19,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-
 
 app.UseAuthorization();
 
